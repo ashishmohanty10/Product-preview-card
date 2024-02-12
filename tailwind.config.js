3f2a8264dc6ad6cd1bb434cbfbfa34f78e89139d
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-02-12 23:24:09 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+
     extend: {
       fontFamily: {
         monsterat: "Montserrat",
         fraunces: "Fraunces",
       },
       colors: {
-        darkCyan: "hsl(158, 36%, 37%)",
+        darkCyan: "hsl(157, 44%, 17%)",
+        Cyan: "hsl(158, 36%, 37%)",
         creame: "hsl(30, 38%, 92%)",
         verDarkBlue: "hsl(212, 21%, 14%)",
         darkGraishBlue: "hsl(228, 12%, 48%)",
